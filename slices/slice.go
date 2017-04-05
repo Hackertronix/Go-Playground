98ed12b9ae113ba14b79ecaa9f07616154fcf9d9
Prateek Phoenix <leavethemails@gmail.com>
2017-04-05 20:46:44 +0530

    completed slice lesson

---
+++ slices/slice.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	s := []string{}
+
+	r := make([]string, 3)
+	fmt.Println(s)
+	fmt.Println(r)
+
+	r[0] = "hello"
+	r[1] = "hi"
+	r[2] = "wadup"
+
+	fmt.Println(r[0])
+}
