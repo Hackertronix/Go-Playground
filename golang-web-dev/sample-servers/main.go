7239bba2c53718237ccca0cf32545951c5bcc6f6
Prateek Phoenix <leavethemails@gmail.com>
2017-04-18 23:58:34 +0530

    bug fix

--- golang-web-dev/sample-servers/main.go
+++ golang-web-dev/sample-servers/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"net"
+)
+
+func main() {
+
+	listener, err := net.Listener("tcp", "localhost:8080")
+	if err != nil{
+		log.Fatal(err)
+
+}
+
+for{
+ conn,err := listener.Accept()
+ if err != nil{
+	log.Print(err)
+	continue
+	
+
+}
+
+}
